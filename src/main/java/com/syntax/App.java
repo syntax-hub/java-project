5b3cbbbb868791ab3114b5872e421ee2d3d6ab53
root <root@ip-172-31-20-218.us-east-2.compute.internal>
2020-07-10 14:26:05 +0000

    initial commit

---
+++ src/main/java/com/syntax/App.java
@@ -0,0 +1,13 @@
+package com.syntax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
